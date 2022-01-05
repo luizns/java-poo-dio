bf1659c4b644802e808cf1d8ac2634b6e645cc09
Luiz Nascimento <luns.wm@hotmail.com>
2022-01-05 20:54:46 -0300

    Abstração, encapsulamento e Bootcamp, Conteudo, Dev classes criadas

--- src/com/github/luizns/poo/dominio/Curso.java
+++ src/com/github/luizns/poo/dominio/Curso.java
@@ -1,28 +1,14 @@
 package com.github.luizns.poo.dominio;
 
-public class Curso {
-
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
     private int cargaHoraria;
 
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
     public Curso() {
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     public int getCargaHoraria() {
@@ -36,8 +22,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
